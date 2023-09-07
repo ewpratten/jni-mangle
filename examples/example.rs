37d9a26ce06c7d8debd7f81eb36080e3ac567515
Evan Pratten <ewpratten@gmail.com>
2023-09-07 12:49:46 -0400

    Ignore error for cleanliness

--- examples/example.rs
+++ examples/example.rs
@@ -1,15 +1,16 @@
 use jni_mangle::mangle;
 
-/// This function automatically generates the mangled name. 
-/// 
+/// This function automatically generates the mangled name.
+///
 /// It is also exposed to *both* Java and Rust
+#[allow(improper_ctypes_definitions)] // For demonstration purposes only
 #[mangle(package = "com.example", class = "Example")]
 pub fn my_rust_function(context: &str) {
     println!("my_rust_function ({})", context);
 }
 
 /// This function is only exposed to Java (although determined programmers can call it directly)
-#[mangle(package = "com.example", class = "Example", alias=false)]
+#[mangle(package = "com.example", class = "Example", alias = false)]
 pub fn function_for_java() {
     println!("Called function_for_java");
 }
@@ -21,12 +22,11 @@ pub fn add_two_numbers(a: i32, b: i32) -> i32 {
 }
 
 pub fn main() {
-
     // my_rust_function is available as both a rust function and a java one
     Java_com_example_Example_my_1rust_1function("Called using mangled name");
     my_rust_function("Called using rust name");
 
-    // While `function_for_java` is only available as a java function, 
+    // While `function_for_java` is only available as a java function,
     // it is technically still possible to call from Rust too
     Java_com_example_Example_function_1for_1java();
 
@@ -35,5 +35,4 @@ pub fn main() {
     println!("add_two_numbers (Java name) = {}", nums);
     let nums = add_two_numbers(1, 2);
     println!("add_two_numbers (Rust name) = {}", nums);
-
 }
